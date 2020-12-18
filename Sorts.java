48fc3fc21476895e5c0f45223982c81caac52c21
Angela Cai <angelacai@Acais-Mac.T-mobile.com>
2020-12-18 00:51:49 -0500

    I think it works

--- Sorts.java
+++ Sorts.java
@@ -5,11 +5,14 @@ public class Sorts{
   */
   public static void bubbleSort(int[] data){
       for (int i = 0; i < data.length-1; i++) {
-          if (data[i] > data[i+1]) {
-             int temp = data[i];
-             data[i]=data[i+1];
-             data[i+1]=temp;
+          for (int j = 0; j < data.length-1-i; j++){
+              if (data[j] > data[j+1]) {
+                 int temp = data[j];
+                 data[j]=data[j+1];
+                 data[j+1]=temp;
+              }
           }
+
       }
   }
 
